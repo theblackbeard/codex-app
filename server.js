6da595c45ff0b8b85d98daba86f7e9589c715fa9
theblackbeard <ccarvalhos@outlook.com>
2017-02-11 16:54:23 -0200

    Ok

--- server.js
+++ server.js
@@ -19,5 +19,7 @@ app.use(morgan('dev'));
 app.use('/api', routes);
 
 app.use(passport.initialize());
-app.listen(3000);
-console.log('Listening on port 3000');
+app.listen(config.c.PORTSERVER, config.c.IPSERVER, () => {
+    console.log('Listening on port ' + config.c.PORTSERVER);
+});
+
