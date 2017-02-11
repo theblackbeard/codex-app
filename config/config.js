fcc1a1b342ebea3f13ad16ae68e2ccc0a4a1cfeb
theblackbeard <ccarvalhos@outlook.com>
2017-02-11 16:41:08 -0200

    Codex Opneshift

--- config/config.js
+++ config/config.js
@@ -3,8 +3,8 @@ const path = require('path');
 let _config = {
     'IPSERVER' : process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
     'PORTSERVER': process.env.OPENSHIFT_NODEJS_PORT || 8001,
-    'MONGO': process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/',
-    'MONGODB': 'series',
+    'MONGO': process.env.MONGODB_URL, // || 'mongodb://127.0.0.1:27017/',
+    'MONGODB': 'codex',
     'ROOT' : path.join(__dirname, '..'),
     'SECRET': 'iknowthisisamazingsysteminnodejsmybabelol'
 }
